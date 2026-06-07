0dfdb54d3220b74a63663dc7f505b71b5e3d636c
Miguel A. Pascual <mianpascual@gmail.com>
2026-06-07 20:03:10 +0200

    Integrate Rest Countries API

--- backend/src/main/java/com/mikepaskual/traveltracker/client/RestCountriesClient.java
+++ backend/src/main/java/com/mikepaskual/traveltracker/client/RestCountriesClient.java
@@ -1,9 +1,11 @@
 package com.mikepaskual.traveltracker.client;
 
+import com.mikepaskual.traveltracker.client.dto.RestCountryResponse;
 import com.mikepaskual.traveltracker.dto.CountryDto;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -16,9 +18,28 @@ public class RestCountriesClient {
     }
 
     public List<CountryDto> getCountries() {
-        return List.of(
-                new CountryDto("ES", "Spain", "Madrid", "Europe", "https://flagcdn.com/es.svg"),
-                new CountryDto("FR", "France", "Paris", "Europe", "https://flagcdn.com/fr.svg")
+        RestCountryResponse[] response =
+                restClient.get()
+                    .uri("https://restcountries.com/v3.1/all?fields=name,flags,region,cca2,capital")
+                    .retrieve()
+                    .body(RestCountryResponse[].class);
+        if (response == null) {
+            return List.of();
+        }
+        return Arrays.stream(response)
+                .map(this::toCountryDto)
+                .toList();
+    }
+
+    private CountryDto toCountryDto(RestCountryResponse country) {
+        return new CountryDto(
+                country.cca2(),
+                country.name().common(),
+                country.capital() != null && !country.capital().isEmpty()
+                        ? country.capital().get(0)
+                        : "",
+                country.region(),
+                country.flags().svg()
         );
     }
 }
